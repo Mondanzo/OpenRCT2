345d59409485d7e4373942e10761a61f718126c1
Ted John <ted@brambles.org>
2017-03-04 19:37:12 +0000

    Add get ride function test

--- src/openrct2/interface/Scripting.cpp
+++ src/openrct2/interface/Scripting.cpp
@@ -16,10 +16,56 @@
 
 #include <../duktape/duktape.h>
 #include "../core/Exception.hpp"
+#include "../core/Memory.hpp"
+#include "../core/String.hpp"
 
 extern "C"
 {
+    #include "../localisation/localisation.h"
+    #include "../ride/ride.h"
     #include "console.h"
+}
+
+static int bind_get_ride(duk_context * ctx)
+{
+    sint32 numArgs = duk_get_top(ctx);
+    if (numArgs == 0)
+    {
+        return DUK_RET_TYPE_ERROR;
+    }
+
+    sint32 index = duk_to_number(ctx, 0);
+
+    rct_ride * ride = nullptr;
+    if (index >= 0 && index < MAX_RIDES)
+    {
+        ride = get_ride(index);
+        if (ride->type == RIDE_TYPE_NULL)
+        {
+            ride = nullptr;
+        }
+    }
+
+    if (ride == nullptr)
+    {
+        duk_push_null(ctx);
+    }
+    else
+    {
+        utf8 rideName[128];
+        format_string(rideName, sizeof(rideName), ride->name, &ride->name_arguments);
+
+        duk_idx_t objidx = duk_push_object(ctx);
+        duk_push_string(ctx, rideName);
+        duk_put_prop_string(ctx, objidx, "name");
+        duk_push_int(ctx, ride->excitement);
+        duk_put_prop_string(ctx, objidx, "excitement");
+        duk_push_int(ctx, ride->intensity);
+        duk_put_prop_string(ctx, objidx, "intensity");
+        duk_push_int(ctx, ride->nausea);
+        duk_put_prop_string(ctx, objidx, "nausea");
+    }
+    return 1;
 }
 
 class ScriptingContext
@@ -35,6 +81,8 @@ public:
         {
             throw Exception("Unable to initialise duktape context.");
         }
+
+        RegisterFunction("GetRide", bind_get_ride);
     }
 
     ~ScriptingContext()
@@ -51,16 +99,34 @@ public:
         }
         else
         {
-            std::string result = duk_to_string(_context, -1);
-            ConsoleWriteLine(result);
+            auto type = duk_get_type(_context, -1);
+            if (type == DUK_TYPE_OBJECT)
+            {
+                std::string result = duk_json_encode(_context, -1);
+                ConsoleWriteLine(result);
+            }
+            else
+            {
+                std::string result = duk_to_string(_context, -1);
+                ConsoleWriteLine(result);
+            }
         }
         duk_pop(_context);
     }
 
 private:
+    void RegisterFunction(const std::string s, duk_c_function function)
+    {
+        duk_push_c_function(_context, function, DUK_VARARGS);
+        duk_put_global_string(_context, s.c_str());
+    }
+
     void ConsoleWriteLine(std::string s)
     {
-        console_writeline(s.c_str());
+        utf8 * text = String::Duplicate(s.c_str());
+        utf8_remove_format_codes(text, false);
+        console_writeline(text);
+        Memory::Free(text);
     }
 
     void ConsoleWriteLineError(std::string s)
