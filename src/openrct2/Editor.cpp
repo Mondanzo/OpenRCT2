c45519ba46e2d99d7b789acfe024be11262cce7e
Ted John <ted@brambles.org>
2019-07-21 20:44:19 +0100

    Do not use user strings for peep names

--- src/openrct2/Editor.cpp
+++ src/openrct2/Editor.cpp
@@ -310,7 +310,7 @@ namespace Editor
             auto peep = get_sprite(i)->AsPeep();
             if (peep != nullptr)
             {
-                user_string_free(peep->name_string_idx);
+                peep->SetName({});
             }
         }
 
